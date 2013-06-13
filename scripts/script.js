6d36f9472c2837395f3ee604cda6072eb375375c
chengmu <lin.zhu.ak@gmail.com>
2013-06-13 18:46:17 +0800

    ADd

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-// 添加格式化日期方法
-(function () {
-	if (typeof Date.prototype.getTidyDate === 'function') return;
-	Date.prototype.getTidyDate = function () {
-		return this.getMonth() + 1 + '/' + this.getDate() + '/' + this.getFullYear();
-	};
-})();
