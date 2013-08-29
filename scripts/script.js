9873d6fb4914d6e7eebb169bb6f088d9fec20ab0
chengmu <lin.zhu.ak@gmail.com>
2013-08-29 21:54:11 +0800

    ADD:ie detection and error page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
+(function () {
+
+
+var ieV = (function isIE () {
+  var myNav = navigator.userAgent.toLowerCase();
+  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+})();
+
+
+//ie 下弹窗
+if (ieV && ieV === 8) {
+    var ieTips = document.createElement('div');
+
+    ieTips.style.position = 'fixed';
+    ieTips.style.top = '0';
+    ieTips.style.color = 'red';
+    ieTips.style.background = 'yellow';
+    ieTips.style.width = '100%';
+    ieTips.style.textAlign = 'center';
+
+
+    ieTips.innerHTML = ['不好意思，因为窝还没有很仔细的做兼容而你刚好用的是IE浏览器或者山寨浏览器的兼容模式，所以这里看起来应该挺惨的。。。不过窝真的好懒得做兼容啊，IE这么讨厌的东西。。Chrome浏览器多好啊_(:з」∠)_'];
+
+    document.getElementsByTagName('body')[0].appendChild(ieTips);
+
+}
+
+
+//加密
 EventUtil = {
     addHandler: function( element, type, handler ) {
         if ( element.addEventListener ) {
@@ -44,3 +73,6 @@ EventUtil.addHandler(btn, 'click', function (e) {
     }
     EventUtil.preventDefault(e);
 });
+
+
+})();
